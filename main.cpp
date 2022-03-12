30110c20c5e652eb5cd676fbab17a154a8ce521a
Canhassi <arthurcanhassi@gmail.com>
2022-03-12 01:52:56 -0300

    basic

---
+++ main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int (z);
+
+    cout<<"welcome, that its the world of whisky, and here"<<endl; 
+    cout<<"you gonna learn all what you want about whisky" <<endl;
+
+    cout<<"type anything to continue" <<endl;
+    cin>> z;
+
+    cout<<"what do you want know?" <<endl;
+    cout<<"1:history and beginning of everything"<<endl;
+    cout<<"2:tips of whisky"<<endl;
+    cout<<"3:rank of the best"<<endl;
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    return 0;
+}
