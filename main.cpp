006565698daf8394190a1a10f83ab155a0ecdd4b
Canhassi <arthurcanhassi@gmail.com>
2022-03-23 00:00:08 -0300

    fix the bug

--- main.cpp
+++ main.cpp
@@ -1,24 +1,36 @@
 #include <iostream>
 using namespace std;
 
+ void opc1()
+ {
+    cout<<"opa" <<endl; 
+ }
+
 int main()
 {
-    int (z);
+    int z,opc;
 
     cout<<"welcome, that its the world of whisky, and here"<<endl; 
     cout<<"you gonna learn all what you want about whisky" <<endl;
-
     cout<<"type anything to continue" <<endl;
-    cin>> z;
+
+    system("pause");
 
     cout<<"what do you want know?" <<endl;
-    cout<<"1:history and beginning of everything"<<endl;
-    cout<<"2:tips of whisky"<<endl;
-    cout<<"3:rank of the best"<<endl;
+    cout<<"1 :history and beginning of everything"<<endl;
+    cout<<"2 :tips of whisky"<<endl;
+    cout<<"3 :rank of the best"<<endl;
 
+    cin>>opc;
     
-    
-    
+
+    switch (opc)
+    {  
+        case 1:
+            opc1();
+            break;
+    }
+        
     
     
     
